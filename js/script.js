6e416d7eaec21e931ad06baf6021157be1d8f80d
Sylvia Ely <sylvianne9417@gmail.com>
2022-10-14 18:34:06 -0700

    Addding pseudocode to JS

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// add javascript
+//start with button logic. 
+//1. When Press Start, timer starts.  
+    //create click
+//2. How to add questions? How many questions do I need? Where do I store them? 
+//3. Where do I store correct answer to question? Click again? Or is a click not needed? Lots of questions here.  
+//4. How to handle incorrect response by subtracting time.  
+//5. How to keep score? How do we hold the high score on page? What is the high score? 1 pt per correct question in each game?
+//6. Game Over event when time hits 0 or questions answered.  
