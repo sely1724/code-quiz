66c6781cdc8aa3dd915a277d36b00f1b8fead14e
Sylvia Ely <sylvianne9417@gmail.com>
2022-10-16 16:56:10 -0700

    did a bunch of research to figure out how to model a question loop.  Now have to add in tracking

--- js/script.js
+++ js/script.js
@@ -1,117 +1,83 @@
-// add javascript
-//start with button logic. 
-//1. When Press Start, timer starts.  
-    //create click
-
-//create timer function
-//var userScore = "";
-//var highScore = "";
-//var totalSeconds = 60;
-//var timeEl = document.querySelector("#time");
-/*function timeCountDown(){
-//for each interval, function executes
-setInterval(function() {
-    totalSeconds--;
-    timeEl.textContent = totalSeconds + " seconds remaining";
-    if(totalSeconds === 0) {
-      clearInterval();
+var question = {
+    question: "What does HTML stand for?",
+    possibleAnswers: ["Hypertext Processing Language", "Hyperfile Programming Learning", "Humans Phone Log"],
+    correctAnswer: 0
     }
-  }, 1000);
-}*/
- 
-//var startQuiz= document.querySelector("#start-quiz");
-
-//startQuiz.addEventListener("click", function (event)){}
-//create timer function
-//setTimer();
-
-
-
-
-
-
-
-
-//2. How to add questions? How many questions do I need? Where do I store them? 
-//3. Where do I store correct answer to question? Click again? Or is a click not needed? Lots of questions here.  
-//4. How to handle incorrect response by subtracting time.  
-//5. How to keep score? How do we hold the high score on page? What is the high score? 1 pt per correct question in each game?
-//store high scores
-/*var checkHighScore = document.querySelector("#high-score");
-
-checkHighScore.addEventListener("click", function (event)) {}
-*/
-
-
-//6. Game Over event when time hits 0 or questions answered. 
-
-
-
-
-//Store Questions as objects
-
-
 
 
 var questionArray = [
-   {question: "What does HTML stand for?",
-    possibleAnswers: ["Hypertext Processing Language", "Hyperfile Programming Learning", "Humans Phone Log"],
-    correctanswer: 0
-    },
-   {question: "What's Sylvia's fav food?",
+    {
+     question: "What does HTML stand for?",
+     possibleAnswers: ["Hypertext Processing Language", "Hyperfile Programming Learning", "Humans Phone Log"],
+     correctAnswer: 0
+     },
+    {
+    question: "What's Sylvia's fav food?",
     possibleAnswers: ["Pizza", "Lox on a Poppyseed Bagel", "Panang Curry"],
-    correctanswer: 1
-    },
-   {question: "What color cat is Simba",
+    correctAnswer: 1
+     },
+    {
+    question: "What color cat is Simba",
     possibleAnswers: ["Black", "Orange", "White"],
-    correctanswer: 1
+    correctAnswer: 1
+     }];
+    
+
+ // define the object for the question entity
+  var app = {
+    start: function() {
+     this.currPosition = 0;
+      // get options
+    
+      var alts = document.querySelectorAll('.option');
+        alts.forEach((element, index) => {
+        element.addEventListener("click", () => {
+          // check correct answer
+          this.checkAnswer(index);
+        });
+      });
+    
+      // show first question
+      this.showQuestion(questionArray[this.currPosition]);
+    },
+    showQuestion: function(q) {
+        // show question
+        var questionAsked= document.getElementById('title');
+        questionAsked.textContent = q.question; 
+            
+            // show options
+        var alts = document.querySelectorAll('.option');
+        alts.forEach(function(element, index){
+         element.textContent = q.possibleAnswers[index];
+            });    
+            }, 
+    checkAnswer: function(userSelected){
+        var currQuestion = questionArray[this.currPosition];
+        if(currQuestion.correctAnswer == userSelected) {
+        console.log("correct");
+        }
+        else{
+        console.log("incorret");
+        }
+
+        this.increasePosition();
+        this.showQuestion(questionArray[this.currPosition]);
+
+    },
+        //increas position 
+    increasePosition: function() {
+        this.currPosition++;
+
+
+        if(this.currPosition == questionArray.length){
+            // send back to the beginning
+            this.currPosition = 0;
     }
-];
-/*
-question.forEach(function(index){
 
-askQuestion(question[index]);
-
-
-
-forEach(function(element,index){
-
-    element.addEventListener("click", function(){
-        if(q.correctanswer == index){
-            console.log("correct");
-            element.style.color = "#90EE90";
-        }
-        else{element.style.color = "red";}
-    });
- });
-}
-
-});*/
+    
+  }   };
+  
+  // initialize the application
+  app.start();
 
 
-
-var questionAsked = document.getElementById("title");
-var options = document.querySelectorAll(".alt");
-
-
-function askQuestion() {
-    questionAsked.textContent = questionArray[1].question;
-
-    options.forEach(function(element,index){
-      element.textContent = questionArray[1].possibleAnswers[index];
-      element.addEventListener("click", function(){
-        if(questionArray[1].correctanswer == index){
-            console.log("correct");
-            element.style.color = "#90EE90";
-        }
-        else{element.style.color = "red";}
-    });
-
-
-
-    })
-};
-
-
-
-askQuestion();
