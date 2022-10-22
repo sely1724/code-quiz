7d71156e335b84bd62f19d466dba9dae87cf01a6
Sylvia Ely <sylvianne9417@gmail.com>
2022-10-22 16:44:43 -0700

    figured out how to subtract 10seconds off time

--- js/script.js
+++ js/script.js
@@ -1,140 +1,138 @@
 
-
 var questionArray = [
-    {
-     question: "What does HTML stand for?",
-     possibleAnswers: ["Hypertext Processing Language", "Hyperfile Programming Learning", "Humans Phone Log"],
-     correctAnswer: 0
-     },
-    {
-    question: "What's Sylvia's fav food?",
-    possibleAnswers: ["Pizza", "Lox on a Poppyseed Bagel", "Panang Curry"],
-    correctAnswer: 1
-     },
-    {
-    question: "What color cat is Simba",
-    possibleAnswers: ["Black", "Orange", "White"],
-    correctAnswer: 1
-     }];
+  {
+   question: "What does HTML stand for?",
+   possibleAnswers: ["Hypertext Processing Language", "Hyperfile Programming Learning", "Humans Phone Log"],
+   correctAnswer: 0
+   },
+  {
+  question: "What's Sylvia's fav food?",
+  possibleAnswers: ["Pizza", "Lox on a Poppyseed Bagel", "Panang Curry"],
+  correctAnswer: 1
+   },
+  {
+  question: "What color cat is Simba",
+  possibleAnswers: ["Black", "Orange", "White"],
+  correctAnswer: 1
+   }];
 
 //initialize outside     
-    
+  
 var timerElement = document.getElementById("time");
 var timerCount = 60;
 var initializeTimer;
-
-
-
 
 var texttobechanged;
 
 
 
-function startTimer(timerElement, timerCount, initializeTimer){
-    initializeTimer = setInterval(function() {
-      timerCount--;
-      timerElement.textContent = timerCount;
-      if (timerCount >= 0) {
-        // Tests if win condition is met
-        console.log("stillgoing")
-        }
-      
-      // Tests if time has run out
-      if (timerCount === 0) {
-        // Clears interval
-        clearInterval(initializeTimer);
-
+function startTimer(){
+  timerElement.textContent = timerCount;
+  initializeTimer = setInterval(function() {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (timerCount >= 0) {
+      // Tests if win condition is met
+      console.log("stillgoing")
       }
-    }, 1000);
-  }
-
-
-
-
-
-
-
-     
- // define the object for the question entity
-  var app = {
-    start: function() {
-     this.currPosition = 0;
-     this.yourScore = 0;
     
-     this.questionAsked= document.getElementById('title');
-    
-      // get options
-      this.updateStats(this.yourScore);
-      var alts = document.querySelectorAll('.option');
-        alts.forEach((element, index) => {
-        element.addEventListener("click", () => {
-          // check correct answer
-          this.checkAnswer(element,index);
-        });
+    // Tests if time has run out
+    if (timerCount === 0) {
+      // Clears interval
+      clearInterval(initializeTimer);
+
+    }
+  }, 1000);
+}
+
+
+
+
+
+
+
+   
+// define the object for the question entity
+var app = {
+  start: function() {
+   this.currPosition = 0;
+   this.yourScore = 0;
+  
+   this.questionAsked= document.getElementById('title');
+  
+    // get options
+    this.updateStats(this.yourScore);
+    var alts = document.querySelectorAll('.option');
+      alts.forEach((element, index) => {
+      element.addEventListener("click", () => {
+        // check correct answer
+        this.checkAnswer(element,index);
       });
-      this.showQuestion(questionArray[this.currPosition]);
+    });
+    this.showQuestion(questionArray[this.currPosition]);
 
-    },
-
-
-    showQuestion: function(q) {
-        // show question
-        
-        this.questionAsked.textContent = q.question; 
-       
-        var alts = document.querySelectorAll('.option');
-        setTimeout(() => {
-          alts.forEach(function(element, index){
-            element.style.color = "black"
-            element.textContent = q.possibleAnswers[index];
-               })
-              
-              },100)}, 
+  },
 
 
-    checkAnswer: function(elementClickedOn, index){
-        var correct;
-        var currQuestion = questionArray[this.currPosition];
-        if(currQuestion.correctAnswer == index) {
-        this.texttobechanged = currQuestion.possibleAnswers[index];
-        console.log("correct");
-        correct = true;
-        this.yourScore+= 5;
-        this.updateStats(this.yourScore); 
-        }
-        else{
-        console.log("incorrect");
-        correct = false;        
-        }
-        this.flashTextColor(elementClickedOn, correct);
-        this.increasePosition();
-        this.showQuestion(questionArray[this.currPosition]);
-    },
-    updateStats: function(yourScore){
-      var score = document.getElementById('points-earned');
-        score.textContent = "Your Score: " + yourScore; 
-    },
-    flashTextColor: function(elementClickedOn, correct){
-      if (correct == true) {
-        //elementClickedOn.style.color = "#90EE90";
-        elementClickedOn.style.color = "#90EE90";
-        }
-      else {
-        // setTimeout(() => {
-        //   elementClickedOn.style.color = "red";
-        // }, 500);
-        elementClickedOn.style.color = "red";
-    }},
-
-        //increase position 
-    increasePosition: function() {
-      this.currPosition++; 
-      if(this.currPosition == questionArray.length){
-      return;
+  showQuestion: function(q) {
+      // show question
       
-    }
-    }
+      this.questionAsked.textContent = q.question; 
+     
+      var alts = document.querySelectorAll('.option');
+      setTimeout(() => {
+        alts.forEach(function(element, index){
+          element.style.color = "black"
+          element.textContent = q.possibleAnswers[index];
+             })
+            
+            },100)}, 
+
+
+  checkAnswer: function(elementClickedOn, index){
+      var correct;
+      var currQuestion = questionArray[this.currPosition];
+      if(currQuestion.correctAnswer == index) {
+      this.texttobechanged = currQuestion.possibleAnswers[index];
+      console.log("correct");
+      correct = true;
+      this.yourScore+= 5;
+      this.updateStats(this.yourScore); 
+      }
+      else{
+      console.log("incorrect");
+      correct = false;     
+      timerCount-=10;   
+      }
+      this.flashTextColor(elementClickedOn, correct);
+      this.increasePosition();
+      this.showQuestion(questionArray[this.currPosition]);
+  },
+  updateStats: function(yourScore){
+    var score = document.getElementById('points-earned');
+      score.textContent = "Your Score: " + yourScore; 
+  },
+  flashTextColor: function(elementClickedOn, correct){
+    if (correct == true) {
+      //elementClickedOn.style.color = "#90EE90";
+      elementClickedOn.style.color = "#90EE90";
+      }
+    else {
+      // setTimeout(() => {
+      //   elementClickedOn.style.color = "red";
+      // }, 500);
+      elementClickedOn.style.color = "red";
+  }},
+
+      //increase position 
+  increasePosition: function() {
+    this.currPosition++; 
+    if(this.currPosition == questionArray.length){
+    return;
+    
   }
+  }
+}
 
 
 
@@ -142,24 +140,19 @@ function startTimer(timerElement, timerCount, initializeTimer){
 
 
 function initialize(timerElement, timerCount, initializeTimer){
-  var startButton = document.getElementById("start-quiz");
-  var questionStructure = document.getElementById("question-structure");
-  questionStructure.setAttribute("style", "display:none");
+var startButton = document.getElementById("start-quiz");
+var questionStructure = document.getElementById("question-structure");
+questionStructure.setAttribute("style", "display:none");
 
-  startButton.addEventListener("click", () => {
-    var instructionsStart = document.getElementById("instructions");
-    instructionsStart.setAttribute("style", "display:none");
-    startButton.setAttribute("style", "display:none");
-    questionStructure.setAttribute("style", "display:block");
-    app.start();    
-    startTimer(timerElement, timerCount, initializeTimer);    
-  });
+startButton.addEventListener("click", () => {
+  var instructionsStart = document.getElementById("instructions");
+  instructionsStart.setAttribute("style", "display:none");
+  startButton.setAttribute("style", "display:none");
+  questionStructure.setAttribute("style", "display:block");
+  app.start();    
+  startTimer();    
+});
 
 }
-initialize(timerElement,timerCount, initializeTimer);
-
-
-
-
-
+initialize();
 
